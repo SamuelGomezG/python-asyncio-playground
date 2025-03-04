7b396f733021ea31d664691dd5d242f7fad8ef27
SamuelGomezG <samgomezgu@unal.edu.co>
2025-03-04 18:57:35 -0500

    Tested merge_sort functionality. Everything working fine

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import random
 import asyncio
 from array_sum import array_sum
+from merge_sort import merge_sort
 
 async def main():
-    arr = [float(e) for e in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+    arr = [float(e) for e in range(1, 101)]
     result = await array_sum(arr)
     print(result)
+    
+    random.shuffle(arr)
+    sorted_arr = await merge_sort(arr)
+    print(arr)
+    print(sorted_arr)
 
 # Python 3.7+
 asyncio.run(main())
