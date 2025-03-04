7ea6d4dbce45e04db0aef5711a1b00b474197e10
SamuelGomezG <samgomezgu@unal.edu.co>
2025-03-03 20:03:05 -0500

    Moved array_sum logic to its own file.

---
+++ array_sum.py
@@ -0,0 +1,18 @@
+import asyncio
+
+async def sub_array_sum(array: list[float], start: int, end: int) -> float:
+    s = 0
+    for i in range(start, end):
+        s += array[i]
+    return s
+
+async def array_sum(array: list[float]) -> float:
+    arr_size = len(array)
+    
+    # Split the array into two halves
+    half_size = arr_size >> 1
+    partial_sums = await asyncio.gather(
+      sub_array_sum(array, 0, half_size),
+      sub_array_sum(array, half_size, arr_size)
+    )
+    return partial_sums[0] + partial_sums[1]
