56321502e444673bae8dfef9dd81e38f278ba5af
SamuelGomezG <samgomezgu@unal.edu.co>
2025-03-03 20:04:06 -0500

    Tested array_sum functionality.

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import asyncio
-
-async def sub_array_sum(array: list, start: int, end: int) -> float:
-    return sum(array[start:end])
-
-async def array_sum(array: list) -> float:
-    arr_size = len(array)
-    
-    # Split the array into two halves
-    half = arr_size // 2
-    results = await asyncio.gather(
-        sub_array_sum(array, 0, half),
-        sub_array_sum(array, half, arr_size)
-    )
-    return sum(results)
+from array_sum import array_sum
 
 async def main():
-    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    arr = [float(e) for e in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
     result = await array_sum(arr)
     print(result)
 
