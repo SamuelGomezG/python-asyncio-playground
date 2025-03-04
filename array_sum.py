66d1b17608b402635c2462d1144231cbad7871c4
SamuelGomezG <samgomezgu@unal.edu.co>
2025-03-03 20:06:14 -0500

    Reformatted array_sum file.

--- array_sum.py
+++ array_sum.py
@@ -12,7 +12,7 @@ async def array_sum(array: list[float]) -> float:
     # Split the array into two halves
     half_size = arr_size >> 1
     partial_sums = await asyncio.gather(
-      sub_array_sum(array, 0, half_size),
-      sub_array_sum(array, half_size, arr_size)
+        sub_array_sum(array, 0, half_size),
+        sub_array_sum(array, half_size, arr_size)
     )
     return partial_sums[0] + partial_sums[1]
